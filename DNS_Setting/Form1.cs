edf05093f1ffc5102b9ceec0556612f248fecac4
Hansi7 <hansi-go@163.com>
2014-11-05 23:54:02 +0800

    first commit

---
+++ DNS_Setting/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DNS_Setting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_SetDNS_Click(object sender, EventArgs e)
+        {
+            try 
+	        {
+                string str = (sender as Button).Tag.ToString();
+                System.Text.RegularExpressions.Regex r = new System.Text.RegularExpressions.Regex(@"[0123456789]+");
+                int i = 0;
+
+                if (int.TryParse(r.Match(str).ToString(),out i))
+                {
+                    Control[] c1 = Controls.Find("txt_G"+ i +"_1", true);
+                    Control[] c2 = Controls.Find("txt_G" + i + "_2", true);
+
+                    DnsHelper.SetDnsServers((c1[0] as TextBox).Text, (c2[0] as TextBox).Text);
+                    MessageBox.Show("设置成功");
+                }
+	        }
+	        catch (Exception)
+	        {
+                MessageBox.Show("设置失败");
+	        }
+        }
+
+        private void btn_defult_Click(object sender, EventArgs e)
+        {
+            //.Start("cmd","netsh interface ip set dns name=以太网 dhcp");
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            p.StartInfo.FileName = @"cmd.exe";
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardInput = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardError = true;
+            p.StartInfo.CreateNoWindow = true;
+            p.Start();
+
+            p.StandardInput.WriteLine("netsh interface ip set dns name=以太网 dhcp"); 
+            p.StandardInput.WriteLine();
+            p.StandardInput.AutoFlush = true;
+            p.StandardInput.WriteLine("exit");
+            string strRst = p.StandardOutput.ReadToEnd();
+            p.Close();      
+        }
+    }
+}
---
+++ DNS_Setting/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DNS_Setting
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            System.Security.Principal.WindowsIdentity identity = System.Security.Principal.WindowsIdentity.GetCurrent();
+            System.Security.Principal.WindowsPrincipal principal = new System.Security.Principal.WindowsPrincipal(identity);
+            //判断当前登录用户是否为管理员
+            if (principal.IsInRole(System.Security.Principal.WindowsBuiltInRole.Administrator))
+            {
+                //如果是管理员，则直接运行
+                Application.Run(new Form1());
+            }
+            else
+            {
+                //创建启动对象
+                System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                startInfo.UseShellExecute = true;
+                startInfo.WorkingDirectory = Environment.CurrentDirectory;
+                startInfo.FileName = Application.ExecutablePath;
+                //设置启动动作,确保以管理员身份运行
+                startInfo.Verb = "runas";
+                try
+                {
+                    System.Diagnostics.Process.Start(startInfo);
+                }
+                catch
+                {
+                    return;
+                }
+                //退出
+                Application.Exit();
+            }
+
+        }
+    }
+}
